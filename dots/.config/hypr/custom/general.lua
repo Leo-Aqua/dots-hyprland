ebf3eeaeb7ab61fcfc7e361ff02615a44707f50f
Leo Aqua <contact@leoaqua.de>
2026-05-19 20:05:16 +0200

    Changed layout options and disabled xray in custom

--- dots/.config/hypr/custom/general.lua
+++ dots/.config/hypr/custom/general.lua
@@ -1,4 +1,17 @@
+hl.config({
+    dwindle = {
+        preserve_split = true,
+        smart_split = true,
+        smart_resizing = true
+        -- precise_mouse_move = true,
+    },
+    decoration = {
+        blur = {
+            xray = false,
+        },
 
+    }
+})
 hl.config({
     input = {
         kb_layout = "de",
