6d0c653b7b9f541742310c46cfa72ce4da984e62
Leo Aqua <contact@leoaqua.de>
2026-05-19 20:05:41 +0200

    add plugins to custom

--- dots/.config/hypr/custom/general.lua
+++ dots/.config/hypr/custom/general.lua
@@ -15,7 +15,194 @@ hl.config({
 hl.config({
     input = {
         kb_layout = "de",
-    }
 
-   
+        accel_profile = "flat",
+        scroll_factor = 0.1,
+    },
+
+    binds = {
+        scroll_event_delay = 250,
+    },
+
 })
+
+hl.config { plugin = { dynamic_cursors = {
+
+    -- enables the plugin
+    enabled = true,
+
+    -- sets the cursor behaviour, supports these values:
+    -- tilt    - tilt the cursor based on x-velocity
+    -- rotate  - rotate the cursor based on movement direction
+    -- stretch - stretch the cursor shape based on direction and velocity
+    -- none    - do not change the cursor's behaviour
+    mode = "tilt",
+
+    -- minimum angle difference in degrees after which the shape is changed
+    -- smaller values are smoother, but more expensive for hw cursors
+    threshold = 2,
+
+    -- for mode = "rotate"
+    rotate = {
+
+        -- length in px of the simulated stick used to rotate the cursor
+        -- most realistic if this is your actual cursor size
+        length = 20,
+
+        -- clockwise offset applied to the angle in degrees
+        -- this will apply to ALL shapes
+        offset = 0.0,
+    },
+
+    -- for mode = "tilt"
+    tilt = {
+
+        -- controls how powerful the tilt is, the lower, the more power
+        -- this value controls at which speed (px/s) the full tilt is reached
+        limit = 5000,
+
+        -- relationship between speed and tilt, supports these values:
+        -- linear             - a linear function is used
+        -- quadratic          - a quadratic function is used (most realistic to actual air drag)
+        -- negative_quadratic - negative version of the quadratic one, feels more aggressive
+        -- see `activation` in `src/mode/utils.cpp` for how exactly the calculation is done
+        activation = "negative_quadratic",
+
+        -- time window (ms) over which the speed is calculated
+        -- higher values will make slow motions smoother but more delayed
+        window = 100,
+
+        -- full tilt for each side (°)
+        full = 60,
+    },
+
+    -- for mode = "stretch"
+    stretch = {
+
+        -- controls how much the cursor is stretched
+        -- this value controls at which speed (px/s) the full stretch is reached
+        -- the full stretch being twice the original length
+        limit = 3000,
+
+        -- relationship between speed and stretch amount, supports these values:
+        -- linear             - a linear function is used
+        -- quadratic          - a quadratic function is used
+        -- negative_quadratic - negative version of the quadratic one, feels more aggressive
+        -- see `activation` in `src/mode/utils.cpp` for how exactly the calculation is done
+        activation = "quadratic",
+
+        -- time window (ms) over which the speed is calculated
+        -- higher values will make slow motions smoother but more delayed
+        window = 100,
+    },
+
+    -- configure shake to find
+    -- magnifies the cursor if its is being shaken
+    shake = {
+
+        -- enables shake to find
+        enabled = true,
+
+        -- controls how soon a shake is detected
+        -- lower values mean sooner
+        threshold = 6.0,
+
+        -- magnification level immediately after shake start
+        base = 4.0,
+        -- magnification increase per second when continuing to shake
+        speed = 4.0,
+        -- how much the speed is influenced by the current shake intensity
+        influence = 0.0,
+
+        -- maximal magnification the cursor can reach
+        -- values below 1 disable the limit (e.g. 0)
+        limit = 0.0,
+
+        -- time in milliseconds the cursor will stay magnified after a shake has ended
+        timeout = 2000,
+
+        -- show cursor behaviour `tilt`, `rotate`, etc. while shaking
+        effects = true,
+
+        -- enable ipc events for shake
+        -- see the `ipc` section below
+        ipc = false,
+    },
+
+    -- use hyprcursor to get a higher resolution texture when the cursor is magnified
+    -- see the `hyprcursor` section below
+    hyprcursor = {
+
+        -- use nearest-neighbour (pixelated) scaling when magnifying beyond texture size
+        -- this will also have effect without hyprcursor support being enabled
+        -- 0 - never use pixelated scaling
+        -- 1 - use pixelated when no highres image
+        -- 2 - always use pixelated scaling
+        nearest = 1,
+
+        -- enable dedicated hyprcursor support
+        enabled = true,
+
+        -- resolution in pixels to load the magnified shapes at
+        -- be warned that loading a very high-resolution image will take a long time and might impact memory consumption
+        -- -1 means we use [normal cursor size] * [shake:base option]
+        resolution = -1,
+
+        -- shape to use when clientside cursors are being magnified
+        -- see the shape-name property of shape rules for possible names
+        -- specifying clientside will use the actual shape, but will be pixelated
+        fallback = "clientside",
+    },
+} } }
+
+
+if hl.plugin.hyprglass then
+    local hg = hl.plugin.hyprglass
+
+    hg.config({
+        enabled = true,
+        default_theme = "dark",
+        default_preset = "vibrant",
+        tint_color = 0x8899aa22,
+
+        brightness = 0.9,
+        dark = { brightness = 0.82 },
+        light = { adaptive_boost = 0.5 },
+
+        layers = { enabled = 0 },
+    })
+
+
+
+    -- Presets
+    hg.preset("vibrant", {
+        -- glass_opacity        = 0.2,
+        brightness           = 1,
+        adaptive_dim         = 0,
+        refraction_strength  = 0.4,
+        chromatic_aberration = 0.5,
+        fresnel_strength     = .2,
+        specular_strength    = 0,
+        lens_distortion      = 1.5,
+        vibrancy             = 10,
+        tint_color           = 0x00000070,
+        blur_strength        = .12,
+        blur_iterations      = 5,
+        saturation           = 1.1
+
+    })
+
+    hg.preset("clear", {
+        glass_opacity = 0.8,
+        blur_strength = 1.5,
+        dark = { brightness = 0.7 },
+        light = { brightness = 1.2 },
+    })
+
+    hg.preset("contrasted", {
+        inherits = "high_contrast",
+        contrast = 1.2,
+        adaptive_dim = 1.5,
+        dark = { tint_color = 0x02142aa9 },
+    })
+end
