b596c717ac8f7110de3e44822aa7e45bd4d2a63b
Leo Aqua <contact@leoaqua.de>
2026-08-03 16:48:37 +0200

    fix ls alias bug when inspecting relative directory

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -1,35 +1,35 @@
 # Commands to run in interactive sessions can go here
 if status is-interactive
-    # No greeting
-    set fish_greeting
+# No greeting
+set fish_greeting
 
-    # Use starship
-    function starship_transient_prompt_func
-        starship module character
-    end
-    if test "$TERM" != "linux"
-        starship init fish | source
-        enable_transience
-    end
-    
-    # Colors
-    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-    end
+# Use starship
+function starship_transient_prompt_func
+starship module character
+end
+if test "$TERM" != "linux"
+starship init fish | source
+enable_transience
+end
 
-    # Aliases
-    # kitty doesn't clear properly so we need to do this weird printing
-    alias clear "printf '\033[2J\033[3J\033[1;1H'"
-    alias celar "printf '\033[2J\033[3J\033[1;1H'"
-    alias claer "printf '\033[2J\033[3J\033[1;1H'"
-    alias pamcan pacman
-    alias q 'qs -c ii'
-    if test "$TERM" != "linux"
-        alias ls 'eza --icons'
-    end
-    if test "$TERM" = "xterm-kitty"
-        alias ssh 'kitten ssh'
-    end
+# Colors
+if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+end
+
+# Aliases
+# kitty doesn't clear properly so we need to do this weird printing
+alias clear "printf '\033[2J\033[3J\033[1;1H'"
+alias celar "printf '\033[2J\033[3J\033[1;1H'"
+alias claer "printf '\033[2J\033[3J\033[1;1H'"
+alias pamcan pacman
+alias q 'qs -c ii'
+if test "$TERM" != "linux"
+alias ls 'bash -c "eza $1 --icons"'
+end
+if test "$TERM" = "xterm-kitty"
+alias ssh 'kitten ssh'
+end
 end
 
 # Android stuff
@@ -45,13 +45,13 @@ set -x OLLAMA_ORIGINS "http://*,https://*,onlyoffice://*"
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /opt/miniconda3/bin/conda
-    eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 else
-    if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/opt/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/opt/miniconda3/bin" $PATH
-    end
+if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+. "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+else
+set -x PATH "/opt/miniconda3/bin" $PATH
+end
 end
 # <<< conda initialize <<<
 
