c2a2063049e6fe33c3534dcac58b9c5e0235f698
Leo Aqua <contact@leoaqua.de>
2026-08-04 22:44:13 +0200

    update ls function to include bash wrapper for eza

--- dots/.config/fish/functions/ls.fish
+++ dots/.config/fish/functions/ls.fish
@@ -1,3 +1,3 @@
-function ls --wraps='exa --icons=auto' --description 'alias ls exa --icons=auto'
-exa --icons=auto $argv
+function ls --wraps='bash -c "eza $1 --icons"' --wraps='exa --icons=auto' --description 'alias ls exa --icons=auto'
+    exa --icons=auto $argv
 end
